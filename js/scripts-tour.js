c4bb3e76a79ca3eccce441bdf85d36557bd773f7
Victor Osipov <victorosipov99@mail.ru>
2017-02-08 19:30:37 +0300

    add new page

---
+++ js/scripts-tour.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+$('.owl-carousel-tour').owlCarousel({
+		items: 6,
+		margin: 26,
+		dots: true,
+		loop: true,
+		autoplay: true,
+		margin: 20,
+		nav: false,
+		autoplayTimeout: 4000,
+		smartSpeed: 1000
+});
+});
